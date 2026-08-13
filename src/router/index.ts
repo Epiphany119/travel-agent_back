7ebcb5b09f00129854371be40f190d4decde6119
胡晶鑫 <15876655260@163.com>
2026-08-13 19:55:31 +0800

    travel-agent_front 修复了一些前后端交互功能

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: '/',
-      name: 'Chat',
-      component: () => import('@/views/ChatView.vue')
-    }
+    { path: '/', name: 'Chat', component: () => import('@/views/ChatView.vue') },
+    { path: '/profile', name: 'Profile', component: () => import('@/views/UserProfile.vue') },
+    { path: '/inspirations', name: 'Inspirations', component: () => import('@/views/InspirationView.vue') },
+    { path: '/journeys', name: 'Journeys', component: () => import('@/views/JourneyView.vue') },
+    { path: '/journey-map', name: 'JourneyMap', component: () => import('@/views/JourneyMapView.vue') }
   ]
 })
 
